38957801df4c82038b3f504476cde762efa65d7f
NextLeaves <wp81023729504@gmail.com>
2018-03-10 00:07:48 +0800

    update getfilelength.cs

--- FileOperationApplication/FileOperationApplication/Program.cs
+++ FileOperationApplication/FileOperationApplication/Program.cs
@@ -16,7 +16,7 @@ namespace FileOperationApplication
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new getFileLength());
         }
     }
 }
---
+++ FileOperationApplication/FileOperationApplication/getFileLength.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.IO;
+
+namespace FileOperationApplication
+{
+    public partial class getFileLength : Form
+    {
+        public getFileLength()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog file_Dialog = new OpenFileDialog();
+
+            if (file_Dialog.ShowDialog() == DialogResult.OK)
+            {
+                FileStream stream = File.Open(file_Dialog.FileName, FileMode.Open);
+                MessageBox.Show("文件大小" + stream.Length + "字节", "提示！");
+            }
+        }
+    }
+}
