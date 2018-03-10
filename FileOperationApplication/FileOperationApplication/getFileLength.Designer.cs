4b7ef9998b8c733672ba5443b8919a9957398085
NextLeaves <wp81023729504@gmail.com>
2018-03-10 10:55:26 +0800

    Update 15.1,15.2

--- FileOperationApplication/FileOperationApplication/getFileLength.Designer.cs
+++ FileOperationApplication/FileOperationApplication/getFileLength.Designer.cs
@@ -29,33 +29,208 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.save_btn = new System.Windows.Forms.Button();
+            this.remove_btn = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button9 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(89, 135);
+            this.button1.Location = new System.Drawing.Point(28, 24);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "文件大小";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // Form1
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(28, 79);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "文件扩展名";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(28, 132);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(92, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "文件创建时间";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(28, 180);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(153, 23);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "文件最后一次修改时间";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(164, 24);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(128, 23);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "获取路径非法字符";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(298, 26);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(526, 76);
+            this.textBox1.TabIndex = 6;
+            // 
+            // save_btn
+            // 
+            this.save_btn.Location = new System.Drawing.Point(238, 162);
+            this.save_btn.Name = "save_btn";
+            this.save_btn.Size = new System.Drawing.Size(75, 23);
+            this.save_btn.TabIndex = 7;
+            this.save_btn.Text = "保存文件";
+            this.save_btn.UseVisualStyleBackColor = true;
+            this.save_btn.Click += new System.EventHandler(this.save_btn_Click);
+            // 
+            // remove_btn
+            // 
+            this.remove_btn.Location = new System.Drawing.Point(238, 209);
+            this.remove_btn.Name = "remove_btn";
+            this.remove_btn.Size = new System.Drawing.Size(75, 23);
+            this.remove_btn.TabIndex = 8;
+            this.remove_btn.Text = "删除文件";
+            this.remove_btn.UseVisualStyleBackColor = true;
+            this.remove_btn.Click += new System.EventHandler(this.remove_btn_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(28, 291);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(119, 23);
+            this.button6.TabIndex = 9;
+            this.button6.Text = "创建随机名文件";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(28, 320);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(119, 23);
+            this.button7.TabIndex = 10;
+            this.button7.Text = "创建随机名文件夹";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(3, 93);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(75, 23);
+            this.button8.TabIndex = 11;
+            this.button8.Text = "Save";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.textBox3);
+            this.panel1.Controls.Add(this.textBox2);
+            this.panel1.Controls.Add(this.button9);
+            this.panel1.Controls.Add(this.button8);
+            this.panel1.Location = new System.Drawing.Point(194, 291);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(254, 119);
+            this.panel1.TabIndex = 12;
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(176, 93);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(75, 23);
+            this.button9.TabIndex = 12;
+            this.button9.Text = "Cancle";
+            this.button9.UseVisualStyleBackColor = true;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(3, 66);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(248, 21);
+            this.textBox2.TabIndex = 13;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(3, 3);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(248, 57);
+            this.textBox3.TabIndex = 14;
+            // 
+            // getFileLength
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.ClientSize = new System.Drawing.Size(1197, 436);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.remove_btn);
+            this.Controls.Add(this.save_btn);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
-            this.Name = "Form1";
+            this.Name = "getFileLength";
             this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button save_btn;
+        private System.Windows.Forms.Button remove_btn;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button9;
     }
 }
 
--- FileOperationApplication/FileOperationApplication/getFileLength.cs
+++ FileOperationApplication/FileOperationApplication/getFileLength.cs
@@ -19,6 +19,11 @@ namespace FileOperationApplication
             InitializeComponent();
         }
 
+        /// <summary>
+        /// key words: OpenFileDialog,File,MessageBox,FileStream
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void button1_Click(object sender, EventArgs e)
         {
             OpenFileDialog file_Dialog = new OpenFileDialog();
@@ -27,6 +32,135 @@ namespace FileOperationApplication
             {
                 FileStream stream = File.Open(file_Dialog.FileName, FileMode.Open);
                 MessageBox.Show("文件大小" + stream.Length + "字节", "提示！");
+            }
+        }
+
+        /// <summary>
+        /// key words:OpenFileDialog,LastIndexOf()
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button2_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog file_Dialog = new OpenFileDialog();
+
+            if (file_Dialog.ShowDialog() == DialogResult.OK)
+            {
+                int index = file_Dialog.FileName.LastIndexOf(".");
+                MessageBox.Show("文件后缀：" + file_Dialog.FileName.Substring(index + 1, file_Dialog.FileName.Length - index - 1), "提示！");
+            }
+        }
+
+        /// <summary>
+        /// Key words:OpenFileDialog,FileInfo
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button3_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog file_Dialog = new OpenFileDialog();
+
+            if (file_Dialog.ShowDialog() == DialogResult.OK)
+            {
+                FileInfo fileInfo = new FileInfo(file_Dialog.FileName);
+                MessageBox.Show("文件创建时间：" + fileInfo.CreationTime.ToString(), "提示！");
+            }
+        }
+
+        /// <summary>
+        /// Key words:OpenFileDialog,FileInfo
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button4_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog file_Dialog = new OpenFileDialog();
+
+            if (file_Dialog.ShowDialog() == DialogResult.OK)
+            {
+                FileInfo fileInfo = new FileInfo(file_Dialog.FileName);
+                MessageBox.Show("文件上一次修改时间：" + fileInfo.LastWriteTime.ToString(), "提示！");
+            }
+        }
+
+        /// <summary>
+        /// Key Words:Path,GetInvalidPathChars()
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button5_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            foreach (char item in Path.GetInvalidPathChars())
+            {
+                textBox1.Text += item + " ";
+            }
+            foreach (char item in Path.GetInvalidFileNameChars())
+            {
+                textBox1.Text += item + " ";
+            }
+        }
+
+        /// <summary>
+        /// Key Words:SaveFileDialog,File,Open(),Delete()
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void save_btn_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog save_Dialog = new SaveFileDialog();
+            if (save_Dialog.ShowDialog() == DialogResult.OK)
+            {
+                File.Create(save_Dialog.FileName);
+            }
+        }
+
+        private void remove_btn_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog open_Dialog = new OpenFileDialog();
+            if (open_Dialog.ShowDialog() == DialogResult.OK)
+            {
+                File.Delete(open_Dialog.FileName);
+            }
+        }
+
+
+        /// <summary>
+        /// Key Words:Guid,NewGuid(),FolderBrowserDialog
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button6_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog folder_Dialog = new FolderBrowserDialog();
+            if (folder_Dialog.ShowDialog() == DialogResult.OK)
+            {
+                File.Create(folder_Dialog.SelectedPath + "\\" + Guid.NewGuid() + ".txt");
+            }
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog folder_Dialog = new FolderBrowserDialog();
+            if (folder_Dialog.ShowDialog() == DialogResult.OK)
+            {
+                Directory.CreateDirectory(folder_Dialog.SelectedPath + "\\" + Guid.NewGuid());
+            }
+        }
+
+        /// <summary>
+        /// Key Words:Path,GetTempFileName(),FileInfo,StreamWriter,AppendText()
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button8_Click(object sender, EventArgs e)
+        {
+            textBox2.Text = Path.GetTempFileName();
+            FileInfo info = new FileInfo(textBox2.Text);
+            StreamWriter streamWriter=info.AppendText();
+            if (textBox3.Text.Length != 0)
+            {
+                streamWriter.Write(textBox3.Text);                
             }
         }
     }
